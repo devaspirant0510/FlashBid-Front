af4211480f94600d21bb2ee41fcc64e052137108
devaspirant0510 <seungho020510@gmail.com>
2025-05-29 02:54:57 +0900

    feat: 실시간 경매 상세 페이지 세팅

---
+++ src/pages/auction/realtime/LiveAuctionInfoPage.tsx
@@ -0,0 +1,11 @@
+import {MainLayout} from "@shared/layout";
+
+const LiveAuctionInfoPage = ()=>{
+    return (
+        <MainLayout>
+            상세페이지
+
+        </MainLayout>
+    )
+}
+export default LiveAuctionInfoPage;
