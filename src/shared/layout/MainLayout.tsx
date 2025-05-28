c573a2a9bb7ff14967d8202f29e28771997f94f4
devaspirant0510 <seungho020510@gmail.com>
2025-05-29 02:48:35 +0900

    style: 좌우 여백 설정

--- src/shared/layout/MainLayout.tsx
+++ src/shared/layout/MainLayout.tsx
@@ -1,5 +1,7 @@
 import React, {FC} from "react";
 import {Header} from "@widgets/ui";
+import {Row} from "@shared/ui";
+import Col from "@shared/ui/grid/Column.tsx";
 
 type Props = {
     children: React.ReactNode;
@@ -8,9 +10,13 @@ const MainLayout:FC<Props> = ({children}) => {
     return (
         <>
             <Header/>
-            <div>
-                {children}
-            </div>
+            <Row>
+                <Col span={2}/>
+                <Col span={20}>
+                    {children}
+                </Col>
+                <Col span={2}/>
+            </Row>
         </>
     )
 }
