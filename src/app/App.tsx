334a6a19ac241ea09c52ebb1de0f196077ed699d
sylee0915 <seungyup8@gmail.com>
2025-06-02 09:03:13 +0900

    feat: profile

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,15 +4,18 @@ import {HomePage} from "@pages/home";
 import {RQProvider} from "@app/provider";
 import {AuctionList} from "@pages/auction";
 import LoginPage from "@pages/login/LoginPage.tsx";
+import ProfilePage from "@pages/profile/ProfilePage.tsx";
+
 
 function App() {
     return (
         <RQProvider>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="/" element={<ProfilePage/>}/>
                     <Route path="/auction-list" element={<AuctionList/>}/>
                     <Route path="/Login" element={<LoginPage/>}/>
+                    <Route path="/Profile" element={<ProfilePage/>}/>
                 </Routes>
             </BrowserRouter>
         </RQProvider>
