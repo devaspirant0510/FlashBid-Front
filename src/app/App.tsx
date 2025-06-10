e384b057fe3b2320156c21f361b3b1cd2e02a2eb
adkslkagl <bongpro1227@gmail.com>
2025-06-10 16:59:11 +0900

    a

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,6 +9,7 @@ import FeedPage from "@pages/feed/FeedPage.tsx";
 import {LiveAuctionInfoPage, LiveAuctionPage} from "@pages/auction/realtime";
 import "./global.css"
 import FeedInfo from "@pages/feed/component/FeedInfo.tsx";
+import RegisterPage from "@/pages/register/RegisterPage";
 
 function App() {
     return (
@@ -23,6 +24,7 @@ function App() {
                     <Route path="/auction/live/:id" element={<LiveAuctionInfoPage/>}/>
                     <Route path="/auction/blind" element={<AuctionList/>}/>
                     <Route path="/FeedInfo/:id" element={<FeedInfo />} />
+                    <Route path={"/register"} element={<RegisterPage/>}/>
                 </Routes>
             </BrowserRouter>
         </RQProvider>
