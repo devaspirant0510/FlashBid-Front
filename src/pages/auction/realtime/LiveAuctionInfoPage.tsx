b76979c7dd01a7c8a2fc1c344259dfb9a09ab3ee
devaspirant0510 <seungho020510@gmail.com>
2025-10-20 16:36:59 +0900

    feat: 경매 페이지 레이아웃 개선 및 조회수 증가 기능 추가

--- src/pages/auction/realtime/LiveAuctionInfoPage.tsx
+++ src/pages/auction/realtime/LiveAuctionInfoPage.tsx
@@ -2,6 +2,7 @@ import { MainLayout } from '@shared/layout';
 import { AuctionInfo, RecommendsGoods } from '@/features/auction/ui';
 import React from 'react';
 import { useParams } from 'react-router';
+import { useIncreaseAuctionView } from '@/features/auction/hooks';
 
 type Params = {
     id: number;
@@ -11,7 +12,7 @@ const LiveAuctionInfoPage = () => {
     if (!id) {
         return <>존재하지 않는 페이지입니다.</>;
     }
-
+    useIncreaseAuctionView(id);
     return (
         <MainLayout>
             <AuctionInfo id={id} type={'live'} />
--- src/pages/auction/realtime/LiveAuctionPage.tsx
+++ src/pages/auction/realtime/LiveAuctionPage.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { MainLayout } from '@shared/layout';
+import { BaseLayout, MainLayout } from '@shared/layout';
 import CategoryLists from '@/features/auction/ui/CategoryLists.tsx';
 import { AuctionList } from '@/features/auction/ui';
 
 const LiveAuctionPage = () => {
     return (
-        <MainLayout>
+        <div>
+            <MainLayout className={'h-0'}>{}</MainLayout>
             <CategoryLists type={'live'} />
-            <AuctionList type={'live'} />
-        </MainLayout>
+            <BaseLayout className={'bg-[#f0f1f5]'}>
+                <AuctionList type={'live'} />
+            </BaseLayout>
+        </div>
     );
 };
 export default LiveAuctionPage;
