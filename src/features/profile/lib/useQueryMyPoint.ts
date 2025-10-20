8f67dc38b30a97ec7fd968d8d52dd1133f17c012
sylee0915 <seungyup8@gmail.com>
2025-10-20 20:37:43 +0900

    feat: 데이터 오류 제거

--- src/features/profile/lib/useQueryMyPoint.ts
+++ src/features/profile/lib/useQueryMyPoint.ts
@@ -1,7 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { httpFetcher } from '@shared/lib';
 import { ApiResult, Page } from '@entities/common';
-import { ChatEntity } from '@entities/auction/model';
 import { PointHistory } from '@entities/payment/model';
 
 export const useQueryMyPoint = () => {
