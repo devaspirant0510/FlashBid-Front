ffbdc684dbef9d9ed93f5cd124b0ef53b7c7e4e1
devaspirant0510 <seungho020510@gmail.com>
2025-10-26 23:30:33 +0900

    fix: 계정 상태 조회 잘못된 쿼리 불러오는 로직 수정

--- src/features/profile/ui/FetchAccountStatus.tsx
+++ src/features/profile/ui/FetchAccountStatus.tsx
@@ -1,13 +1,15 @@
 import React, { FC } from 'react';
 import { UserStats } from '@entities/user/model';
 import { useQueryGetAllAuctionChat } from '@/features/auction/lib';
+import { useQueryGetUserById } from '@/features/user/lib';
+import { useQueryGetAccountStatus } from '@/features/profile/lib/useQueryGetAccountStatus.ts';
 
 type Props = {
     accountId: number;
     children: (data: UserStats) => React.ReactNode;
 };
 const FetchAccountStatus: FC<Props> = ({ accountId, children }) => {
-    const { isLoading, data, isError, error } = useQueryGetAllAuctionChat(accountId);
+    const { isLoading, data, isError, error } = useQueryGetAccountStatus(accountId);
     if (isLoading) {
         return <>loading</>;
     }
