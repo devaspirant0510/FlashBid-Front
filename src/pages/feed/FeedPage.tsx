b1315a3dbbd4eb87f49ee8db2cde6caf25bf872f
devaspirant0510 <seungho020510@gmail.com>
2025-10-28 12:47:33 +0900

    design: 반응형 디자인 처리

--- src/pages/feed/FeedPage.tsx
+++ src/pages/feed/FeedPage.tsx
@@ -4,17 +4,22 @@ import React, { useState } from 'react';
 import { Modal } from '@pages/feed/component/Modal.tsx';
 import { Header } from '@widgets/ui';
 import { AppLayout, BaseLayout } from '@shared/layout';
+import { Column, Row } from '@shared/ui';
 
 const FeedPage = () => {
     const [openModal, setOpenModal] = useState(false);
     return (
         <AppLayout>
-            <BaseLayout>
-                <div className=' top-0 z-10 '>
-                    <WritePost onClick={() => setOpenModal(true)} />
-                </div>
-                <FeedList />
-            </BaseLayout>
+            <Row>
+                <Column md={5} xs={0} />
+                <Column md={14} xs={24}>
+                    <div className=' top-0 z-10 '>
+                        <WritePost onClick={() => setOpenModal(true)} />
+                    </div>
+                    <FeedList />
+                </Column>
+                <Column md={5} xs={0} />
+            </Row>
             {openModal && <Modal onClose={() => setOpenModal(false)} />}
         </AppLayout>
     );
